562b510ab8288cde9864e463bb7011a6a4d9442a
xtarzyk <xtarzyk@tutanota.com>
2024-08-18 20:23:51 +0200

    split components to files, create utils

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import FormAddPost from "./FormAddPost";
+import Posts from "./Posts";
+
+function Main({ posts, onAddPost }) {
+  return (
+    <main>
+      <FormAddPost onAddPost={onAddPost} />
+      <Posts posts={posts} />
+    </main>
+  );
+}
+
+export default Main
---
+++ src/components/Posts.jsx
@@ -0,0 +1,11 @@
+import List from "./List";
+
+function Posts({ posts }) {
+  return (
+    <section>
+      <List posts={posts} />
+    </section>
+  );
+}
+
+export default Posts
